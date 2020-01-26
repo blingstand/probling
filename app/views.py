c18e896a20a0d50b6cac6c02a10b593775256c28
blingstand <blingstand@hotmail.fr>
2020-01-26 10:32:06 +0100

    fourth commit

--- app/views.py
+++ app/views.py
@@ -1,10 +1,31 @@
 from app import app
 
 
+
 @app.route('/')
 def hello():
-    return 'Déploiement validé !'
+    return """
+<!DOCTYPE html>
+ <html lang="en">
+ <head>
+     <meta charset="UTF-8">
+     <title>Hello</title>
+ </head>
+ <body>
+     <h1>Hello World réussi</h1>
+ </body>
+ </html> """
 
 @app.route('/test')
 def test():
-    return 'test validé !'
+    return """
+<!DOCTYPE html>
+ <html lang="en">
+ <head>
+     <meta charset="UTF-8">
+     <title>Test</title>
+ </head>
+ <body>
+     <h1>Test réussi</h1>
+ </body>
+ </html> """
