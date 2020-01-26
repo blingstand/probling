e84385ecd2bd719ef9d214bd34e67ea1f3ba1289
blingstand <blingstand@hotmail.fr>
2020-01-26 11:47:53 +0100

    twelfth commit

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,12 @@
 from flask import Flask
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
+
 
 from app import views
+
+if __name__ == '__main__':
+    app.run()
