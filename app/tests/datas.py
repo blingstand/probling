2d864cfb0811f833ca60dae1993ef2829f64ae13
adrien Clupot <adrien.clupot@gmail.com>
2020-02-25 10:56:40 +0100

    fix bug in test_apipro7.py

---
+++ app/tests/datas.py
@@ -0,0 +1,42 @@
+BIG_EXTRACT = """Bordeaux (prononcé /bɔʁ.do/1 Écouter) est une commune du Sud-Ouest de la France. Capitale de la Gaule aquitaine dès le début du IIIe siècle, puis du duché d'Aquitaine et enfin de l'ancienne province de Guyenne sous l'Ancien régime, elle est aujourd'hui le chef-lieu de la région Nouvelle Aquitaine, préfecture du département de la Gironde et le siège de Bordeaux Métropole."""
+SMALL_EXTRACT = """Bergerac est une commune française située dans le département de la Dordogne, en région Nouvelle-Aquitaine."""
+QUESTIONS = ["où se trouve le pont Chaban-Delmas ?",#1\
+        "Où se trouve le pont Chaban-Delmas ?",\
+        "connais-tu le pont Chaban-Delmas ?",\
+        "Connais-tu le pont Chaban-Delmas ?",\
+        "connaissez-vous le pont Chaban-Delmas ?", #5\
+        "Connaissez-vous le pont Chaban-Delmas ?",\
+        "connais-tu l'adresse du pont Chaban-Delmas ?",\
+        "Connais-tu l'adresse du pont Chaban-Delmas ?",\
+        "connaissez-vous l'adresse du pont Chaban-Delmas ?",\
+        "Connaissez-vous l'adresse du pont Chaban-Delmas ?", #10\
+        "peux-tu me parler du pont Chaban-Delmas ?",\
+        "Peux-tu me parler du pont Chaban-Delmas ?",\
+        "pouvez-vous me parler du pont Chaban-Delmas ?",\
+        "Pouvez-vous me parler du pont Chaban-Delmas ?",\
+        "Que peux-tu me dire sur le pont Chaban-Delmas ?",#15\
+        "Que peux-tu me dire sur le pont Chaban-Delmas qui se trouve à Bordeaux ?",\
+        "Que pouvez-vous me dire sur le pont Chaban-Delmas qui se trouve à Bordeaux ?",\
+        "sais-tu quelque chose à propos du pont Chaban-Delmas ?",\
+        "Sais-tu quelque chose à propos du pont Chaban-Delmas ?", \
+        "savez-vous quelque chose à propos du pont Chaban-Delmas ?",#20\
+        "Savez-vous quelque chose à propos du pont Chaban-Delmas ?",\
+        "sais-tu quelque chose sur le pont Chaban-Delmas ?",\
+        "Sais-tu quelque chose sur le pont Chaban-Delmas ?",\
+        "Savez-vous quelque chose sur le pont Chaban-Delmas ?",\
+        "savez-vous quelque chose sur le pont Chaban-Delmas ?",#25\
+        "as-tu des informations sur le pont Chaban-Delmas ?",\
+        "As-tu des informations sur le pont Chaban-Delmas ?",\
+        "avez-vous des informations sur le pont Chaban-Delmas ?",
+        "Que connais-tu du pont Chaban-Delmas ?",\
+        "Avez-vous des informations sur le pont Chaban-Delmas ?",#30\
+        "Que connais-tu de la Tour Eiffel ?",\
+        "Que connaissez-vous du pont Chaban-Delmas ?",\
+        "Est-ce que tu connais le pont Chaban-Delmas ?",\
+        "Est-ce que tu connais l'adresse du pont Chaban-Delmas ? ",\
+        "Est-ce que vous connaissez le pont Chaban-Delmas ?", #35\
+        "Est-ce que vous connaissez l'adresse du pont Chaban-Delmas ? ",\
+        "Est-ce que vous connaissez l'adresse d'Openclassroom ? ",\
+        "Où se situe le pont Chaban-Delmas ?",\
+        "Où est le pont Chaban-Delmas ?"\
+        ]
